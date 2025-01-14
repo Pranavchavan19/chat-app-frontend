751e7f3b26dfbc8ed7f04eadaff846fa19d0054b
Pranavbc <pranav1952000@gmail.com>
2025-01-14 16:17:04 +0530

    f

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,4 +1,4 @@
-// import { httpClient } from "../config/AxiosHelper"
+import { httpClient } from "../config/AxiosHelper"
 
 // export const createRoomApi = async (roomDetail) => {
 //   const response = await httpClient.post(`/api/v1/rooms`,roomDetail,{
