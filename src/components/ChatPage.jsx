1d7093c914333ce0c9eaaf27290c878477adf635
Pranavbc <pranav1952000@gmail.com>
2025-01-14 15:41:30 +0530

    c

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -886,75 +886,154 @@ const ChatPage = () => {
   }
 
   return (
+    
     <div className="relative">
-      <header className="dark:border-gray-700 fixed w-full dark:bg-gray-900 py-5 shadow flex justify-between items-center px-10 hidden sm:flex">
-        <div>
-          <h1 className="text-xl sm:text-2xl font-semibold">
-            Room: <span>{roomId}</span>
-          </h1>
-        </div>
-        <div>
-          <h1 className="text-xl sm:text-2xl font-semibold">
-            User: <span>{currentUser}</span>
-          </h1>
-        </div>
-        <div>
-          <button
-            onClick={handleLogout}
-            className="dark:bg-red-500 dark:hover:bg-red-700 px-3 py-2 rounded-full text-sm sm:text-base"
+    <header className="dark:border-gray-700 fixed w-full dark:bg-gray-900 py-5 shadow flex justify-between items-center px-10 hidden sm:flex">
+      <div>
+        <h1 className="text-xl sm:text-2xl font-semibold">
+          Room: <span>{roomId}</span>
+        </h1>
+      </div>
+      <div>
+        <h1 className="text-xl sm:text-2xl font-semibold">
+          User: <span>{currentUser}</span>
+        </h1>
+      </div>
+      <div>
+        <button
+          onClick={handleLogout}
+          className="dark:bg-red-500 dark:hover:bg-red-700 px-3 py-2 rounded-full text-sm sm:text-base"
+        >
+          Leave Room
+        </button>
+      </div>
+    </header>
+  
+    <header className="dark:border-gray-700 fixed w-full dark:bg-gray-900 py-5 shadow flex justify-between items-center px-4 sm:hidden">
+      <div>
+        <h1 className="text-sm sm:text-base font-semibold">
+          Room: <span>{roomId}</span>
+        </h1>
+      </div>
+      <div>
+        <h1 className="text-sm sm:text-base font-semibold">
+          User: <span>{currentUser}</span>
+        </h1>
+      </div>
+      <div>
+        <button
+          onClick={handleLogout}
+          className="dark:bg-red-500 dark:hover:bg-red-700 px-3 py-2 rounded-full text-xs sm:text-sm"
+        >
+          Leave Room
+        </button>
+      </div>
+    </header>
+  
+    <main
+      ref={chatBoxRef}
+      className="py-20 px-10 w-2/3 dark:bg-slate-600 mx-auto h-screen overflow-auto hidden sm:block"
+    >
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={`flex ${
+            message.sender === currentUser ? "justify-end" : "justify-start"
+          }`}
+        >
+          <div
+            className={`my-2 ${
+              message.sender === currentUser ? "bg-green-800" : "bg-gray-800"
+            } p-2 max-w-xs rounded`}
           >
-            Leave Room
-          </button>
-        </div>
-      </header>
-
-      <main ref={chatBoxRef} className="py-16 px-4 w-full dark:bg-slate-600 mx-auto h-screen overflow-auto sm:hidden">
-        {messages.map((message, index) => (
-          <div key={index} className={`flex ${message.sender === currentUser ? "justify-end" : "justify-start"}`}>
-            <div className={`my-2 ${message.sender === currentUser ? "bg-green-800" : "bg-gray-800"} p-2 max-w-xs rounded`}>
-              <div className="flex flex-row gap-2">
-                <img className="h-8 w-8" src={"https://avatar.iran.liara.run/public/43"} alt="" />
-                <div className="flex flex-col gap-1">
-                  <p className="text-xs sm:text-sm font-bold">{message.sender}</p>
-                  <p className="text-xs sm:text-sm">{message.content}</p>
-                  <p className="text-xs sm:text-sm text-gray-400">{timeAgo(message.timeStamp)}</p>
-                </div>
+            <div className="flex flex-row gap-2">
+              <img
+                className="h-10 w-10"
+                src={"https://avatar.iran.liara.run/public/43"}
+                alt=""
+              />
+              <div className="flex flex-col gap-1">
+                <p className="text-sm sm:text-base font-bold">
+                  {message.sender}
+                </p>
+                <p className="text-sm sm:text-base">{message.content}</p>
+                <p className="text-xs sm:text-sm text-gray-400">
+                  {timeAgo(message.timeStamp)}
+                </p>
               </div>
             </div>
           </div>
-        ))}
-      </main>
-
-      <div className="fixed bottom-0 left-0 w-full z-10 flex justify-center">
-        <div className="w-full sm:w-3/4 lg:w-2/4 px-4 py-2">
-          <div className="flex items-center justify-between rounded-full w-full">
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  sendMessage();
-                }
-              }}
-              type="text"
-              placeholder="Type your message here..."
-              className="w-full dark:border-gray-600 dark:bg-pink-800 px-3 py-2 rounded-full focus:outline-none text-sm sm:text-base"
-            />
-            <div className="flex gap-2 ml-2">
-              <button className="dark:bg-purple-600 h-8 w-8 flex justify-center items-center rounded-full hover:bg-purple-700 transition-all">
-                <MdAttachFile size={20} />
-              </button>
-              <button
-                onClick={sendMessage}
-                className="dark:bg-green-600 h-8 w-8 flex justify-center items-center rounded-full hover:bg-green-700 transition-all"
-              >
-                <MdSend size={20} />
-              </button>
+        </div>
+      ))}
+    </main>
+  
+    <main
+      ref={chatBoxRef}
+      className="py-16 px-4 w-full dark:bg-slate-600 mx-auto h-screen overflow-auto sm:hidden"
+    >
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={`flex ${
+            message.sender === currentUser ? "justify-end" : "justify-start"
+          }`}
+        >
+          <div
+            className={`my-2 ${
+              message.sender === currentUser ? "bg-green-800" : "bg-gray-800"
+            } p-2 max-w-xs rounded`}
+          >
+            <div className="flex flex-row gap-2">
+              <img
+                className="h-8 w-8"
+                src={"https://avatar.iran.liara.run/public/43"}
+                alt=""
+              />
+              <div className="flex flex-col gap-1">
+                <p className="text-xs sm:text-sm font-bold">{message.sender}</p>
+                <p className="text-xs sm:text-sm">{message.content}</p>
+                <p className="text-xs sm:text-sm text-gray-400">
+                  {timeAgo(message.timeStamp)}
+                </p>
+              </div>
             </div>
+          </div>
+        </div>
+      ))}
+    </main>
+  
+    <div className="fixed bottom-0 left-0 w-full z-10 flex justify-center">
+      <div className="w-full sm:w-3/4 lg:w-2/4 px-4 py-2">
+        <div className="flex items-center justify-between rounded-full w-full">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+            type="text"
+            placeholder="Type your message here..."
+            className="w-full dark:border-gray-600 dark:bg-pink-800 px-3 py-2 rounded-full focus:outline-none text-sm sm:text-base"
+          />
+          <div className="flex gap-2 ml-2">
+            <button className="dark:bg-purple-600 h-8 w-8 flex justify-center items-center rounded-full hover:bg-purple-700 transition-all">
+              <MdAttachFile size={20} />
+            </button>
+            <button
+              onClick={sendMessage}
+              className="dark:bg-green-600 h-8 w-8 flex justify-center items-center rounded-full hover:bg-green-700 transition-all"
+            >
+              <MdSend size={20} />
+            </button>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  
+   
   );
 };
 
