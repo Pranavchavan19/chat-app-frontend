bd56b5a9b4b0ebd48eed2533936e24ebf89d9600
Pranavbc <pranav1952000@gmail.com>
2025-01-15 17:10:32 +0530

    versal.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react()],
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Specify the custom build directory
+  },
+  server: {
+    proxy: {
+      '/api': 'https://chat-app-backend-x9jj.onrender.com',
+    },
+  },
 })
